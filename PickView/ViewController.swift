156a9380a17b3b5a99378827ce36582686f4c7b8
ArtemVasilenko <artemvasilenko88@gmail.com>
2018-12-21 20:41:40 +0200

    commit

--- PickView/ViewController.swift
+++ PickView/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let cityArr = ["Kyiv", "Dnipro", "Lviv", "Kharkiv", "Odessa"]
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var cityPickerView: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.cityPickerView.delegate = self
+        //подписываем наш пикер на делегегаты (изменение данных)
+        //делегейт это выполнение одного класса в другом ()
+        
+        self.cityPickerView.dataSource = self
+        //управление данными
     }
-
-
+    
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    //вынесли в расширение чтобы не был основной вью контроллер забит
+    //вообще надо в отдельный класс
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1        //кол-во столбцов
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.cityArr.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.cityArr[row]        //вносит название строк
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.label.text = self.cityArr[row]
+    }
+    
+}
